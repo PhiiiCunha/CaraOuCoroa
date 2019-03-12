367f6dd893acb46d873f50653ba835a5e700d80f
Philipe Cunha <philipe.cunha92@gmail.com>
2019-03-11 23:23:36 -0300

    Frontend

---
+++ app/src/main/java/com/example/caraoucoroa/activity/ResultadoActivity.java
@@ -0,0 +1,15 @@
+package com.example.caraoucoroa.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.caraoucoroa.R;
+
+public class ResultadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado);
+    }
+}
